48f3a9abb8c07d0fc29b214ae2f6edcf759dbf54
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,46 @@
-output "vpn_server_configurations" {
-  description = "All vpn_server_configuration resources"
-  value       = azurerm_vpn_server_configuration.vpn_server_configurations
-  sensitive   = true
-}
 output "vpn_server_configurations_azure_active_directory_authentication" {
-  description = "List of azure_active_directory_authentication values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.azure_active_directory_authentication]
+  description = "Map of azure_active_directory_authentication values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.azure_active_directory_authentication }
 }
 output "vpn_server_configurations_client_revoked_certificate" {
-  description = "List of client_revoked_certificate values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.client_revoked_certificate]
+  description = "Map of client_revoked_certificate values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.client_revoked_certificate }
 }
 output "vpn_server_configurations_client_root_certificate" {
-  description = "List of client_root_certificate values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.client_root_certificate]
+  description = "Map of client_root_certificate values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.client_root_certificate }
 }
 output "vpn_server_configurations_ipsec_policy" {
-  description = "List of ipsec_policy values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.ipsec_policy]
+  description = "Map of ipsec_policy values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.ipsec_policy }
 }
 output "vpn_server_configurations_location" {
-  description = "List of location values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.location]
+  description = "Map of location values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.location }
 }
 output "vpn_server_configurations_name" {
-  description = "List of name values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.name]
+  description = "Map of name values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.name }
 }
 output "vpn_server_configurations_radius" {
-  description = "List of radius values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.radius]
+  description = "Map of radius values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.radius }
   sensitive   = true
 }
 output "vpn_server_configurations_resource_group_name" {
-  description = "List of resource_group_name values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.resource_group_name]
+  description = "Map of resource_group_name values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.resource_group_name }
 }
 output "vpn_server_configurations_tags" {
-  description = "List of tags values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.tags]
+  description = "Map of tags values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.tags }
 }
 output "vpn_server_configurations_vpn_authentication_types" {
-  description = "List of vpn_authentication_types values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.vpn_authentication_types]
+  description = "Map of vpn_authentication_types values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.vpn_authentication_types }
 }
 output "vpn_server_configurations_vpn_protocols" {
-  description = "List of vpn_protocols values across all vpn_server_configurations"
-  value       = [for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : v.vpn_protocols]
+  description = "Map of vpn_protocols values across all vpn_server_configurations, keyed the same as var.vpn_server_configurations"
+  value       = { for k, v in azurerm_vpn_server_configuration.vpn_server_configurations : k => v.vpn_protocols }
 }
 
